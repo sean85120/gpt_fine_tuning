c86b0fdeeb203a2b04188930edc31e5bf5d97617
Sean <91415054+sean85120@users.noreply.github.com>
2023-09-12 15:13:33 +0800

    modify fine tuning

--- fine_tuning.py
+++ fine_tuning.py
@@ -7,6 +7,10 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+openai.File.create(file=open("mydata.jsonl", "rb"), purpose="fine-tune")
+
+openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+
 
 def fine_tuning():
     print("test")
