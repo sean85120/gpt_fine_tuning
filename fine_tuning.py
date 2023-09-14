8862a6315e3b60188b91b20972bdc2a8a64be119
Sean <91415054+sean85120@users.noreply.github.com>
2023-09-14 16:19:49 +0800

    add poetry and fine tuning validator

--- fine_tuning.py
+++ fine_tuning.py
@@ -3,15 +3,50 @@ import os
 import openai
 from dotenv import load_dotenv
 
+from gen_dataset import gen_dataset
+
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-openai.File.create(file=open("mydata.jsonl", "rb"), purpose="fine-tune")
 
-openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+# estimate cost: 6.27948 usd with 261645 tokens * 3 epochs ( default )
+def fine_tuning(file):
+    training_file = gen_dataset(file)
+
+    fine_tuned_model = openai.FineTuningJob.create(
+        training_file=training_file, model="gpt-3.5-turbo"
+    )
+
+    return fine_tuned_model
+
+    # fine_tune_model: {
+    #   "object": "fine_tuning.job",
+    #   "id": "ftjob-eRw7CTnEWew07tNIp7zSSRTr",
+    #   "model": "gpt-3.5-turbo-0613",
+    #   "created_at": 1694674172,
+    #   "finished_at": null,
+    #   "fine_tuned_model": null,
+    #   "organization_id": "org-6Kd8d957L2nX9ZTcSGSvW9Bq",
+    #   "result_files": [],
+    #   "status": "created",
+    #   "validation_file": null,
+    #   "training_file": "file-Qo2wzwjqaCunIJI1v5uEpvmO",
+    #   "hyperparameters": {
+    #     "n_epochs": 3
+    #   },
+    #   "trained_tokens": null,
+    #   "error": null
+    # }
 
 
-def fine_tuning():
-    print("test")
-    pass
+def fine_tuning_status(model):
+    fine_tuned_model = openai.FineTuningJob.retrieve(model)
+
+    return fine_tuned_model
+
+
+if __name__ == "__main__":
+    model = "ftjob-eRw7CTnEWew07tNIp7zSSRTr"
+    status = fine_tuning_status(model)
+    print("fine_tune_status:", status)
--- gen_dataset.py
+++ gen_dataset.py
@@ -8,5 +8,27 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def gen_dataset():
-    pass
+def gen_dataset(file):
+    training_file = openai.File.create(
+        file=open("柯文哲_dataset_chatglm.jsonl", "rb"), purpose="fine-tune"
+    )
+
+    print("training_file:", training_file)
+
+    return training_file["id"]
+
+
+if __name__ == "__main__":
+    file = "柯文哲_dataset_chatglm.jsonl"
+    dataset = gen_dataset(file)
+
+    # fine_tune_dataset: {
+    #     "object": "file",
+    #     "id": "file-Qo2wzwjqaCunIJI1v5uEpvmO",
+    #     "purpose": "fine-tune",
+    #     "filename": "file",
+    #     "bytes": 580255,
+    #     "created_at": 1694673961,
+    #     "status": "uploaded",
+    #     "status_details": null
+    # }
